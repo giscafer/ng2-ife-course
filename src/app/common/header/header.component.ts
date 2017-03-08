561166e821c6186a11f8719b66a5ec004e05e899
giscafer <giscafer@outlook.com>
2017-03-08 21:29:55 +0800

    修改手机端菜单事件

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -7,9 +7,12 @@ import {Router} from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   public currentSelected: string = '';
+  displayStyle: string;
+  mobine: boolean;
 
   constructor(public router: Router) {
-
+    this.mobine = window.innerWidth > 1000 ? false : true;
+    this.displayStyle = !this.mobine ? 'inline-block' : 'none';
   }
 
   ngOnInit() {
@@ -19,5 +22,20 @@ export class HeaderComponent implements OnInit {
 
   selectMenu(type = 'xiaowei') {
     this.currentSelected = type;
+  }
+
+  phoneMenuClick($event) {
+    if(!this.mobine) return;
+    if ($event.target.nodeName === 'SPAN' && $event.target.id == "main-nav-toggle") {
+      if (this.displayStyle === 'none') {
+        this.displayStyle = 'inline-block'
+      } else {
+        this.displayStyle = 'none'
+      }
+    } else if ($event.target.nodeName === 'SPAN' || $event.target.nodeName === 'LI' && $event.target.id !== "main-nav-toggle") {
+      this.displayStyle = 'none';
+    } else {
+      this.displayStyle = 'inline-block';
+    }
   }
 }
